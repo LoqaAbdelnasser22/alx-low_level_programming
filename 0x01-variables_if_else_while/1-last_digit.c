6e1c0622e7a5658707397b9b09f26ba988f2a9c5
LoqaAbdelnasser22 <loqa.gamal22@gmail.com>
2023-07-16 10:26:20 -0700

    finished

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,13 +5,14 @@
 *main - entry point
 *
 *Description: print the value of n status
-*greater than , is zero and is not less than 6.
+*	greater than , is zero and is not less than 6.
 *
 *Return : 0 (success)
 */
 int main(void)
 {
-int n;
+int n, digit;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 digit = n % 10;
